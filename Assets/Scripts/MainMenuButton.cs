893db5e97327050e1f8240b87d65aa9e9bf9b480
Vincentimes62 <vincent.nardy@piktura.fr>
2024-10-25 11:12:17 +0200

    MainMenu PArt2

--- Assets/Scripts/MainMenuButton.cs
+++ Assets/Scripts/MainMenuButton.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
 
 public class MainMenuButton : MonoBehaviour
 {
     public Image ImageOutline;
+    public TMP_Text UITextMeshPro;
+    public Color InitialColor, OverColor;
 
     public void OnPointerEnter()
     {
-        Color color = ImageOutline.color;
-        color.a = 1;
-        ImageOutline.color = color;
+        ImageOutline.DOKill();
+        ImageOutline.DOFade(1, 0.3f);
+        UITextMeshPro.DOKill();
+        UITextMeshPro.DOColor(OverColor, 0.3f);
+
     }
 
     public void OnPointerExit()
     {
-        Color color = ImageOutline.color;
-        color.a = 0;
-        ImageOutline.color = color;
+        ImageOutline.DOKill();
+        ImageOutline.DOFade(0, 0.2f);
+        UITextMeshPro.DOKill();
+        UITextMeshPro.DOColor(InitialColor, 0.2f);
     }
 }
