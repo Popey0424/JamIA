447cb611fbfcffda1f8564c98473f9470b8548a0
Vincentimes62 <vincent.nardy@piktura.fr>
2024-10-25 11:40:24 +0200

    MainMenu_Part3

--- Assets/Scenes/_Scripts/MainMenu.cs
+++ Assets/Scenes/_Scripts/MainMenu.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
+    public Image ImageFade;
+    public List<MainMenuButton> Buttons;
+
+    private void Start()
+    {
+        ImageFade.DOFade(0, 0);
+        ImageFade.gameObject.SetActive(false);
+    }
     public void OnClickPlay()
     {
-       SceneManager.LoadScene("Gameplay");
+        ImageFade.gameObject.SetActive(true);
+        ImageFade.DOFade(1, 0.8f).OnComplete(LoadGame);
+    }
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene("GameplayLouis");
     }
 }
